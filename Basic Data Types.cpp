e489fef984b1cb2ef2feee5574c34056afc58c1a
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-17 00:42:58 +0530

    Add files via upload

---
+++ Basic Data Types.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+  /*  int n;
+    cin >> n;
+    long long int m;
+    cin >> m;
+    char c;
+    cin >> c;
+    float f;
+    cin >> f;
+    double d;
+    cin >> d;
+
+    cout << n <<endl;
+    cout << m << endl;
+    cout << c << endl;
+    cout << f <<endl;
+    cout << d << endl;*/
+
+
+    int a;
+
+long long int c;
+char d;
+float e;
+double f;
+
+scanf("%d %lld %c %f %lf",&a,&c,&d,&e,&f);
+
+printf("%d\n%lld\n%c\n%f\n%lf",a,c,d,e,f);
+    return 0;
+}
